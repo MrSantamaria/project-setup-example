c2a269491fe0ba03a93e1e7242a823d571ec8d58
Blake Kruppa <btkruppa513@gmail.com>
2017-08-04 16:13:49 -0400

    fixed the project, but i was also testing out $scope scopes

---
+++ app/src/main/java/com/revature/controllers/SPAControllerImpl.java
@@ -0,0 +1,18 @@
+package com.revature.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class SPAControllerImpl {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getSPA() {
+
+		return "index.html";
+
+	}
+
+}
